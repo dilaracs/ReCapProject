03898280db6afcde0d63c9dfdfac1c3c8e4648d6
dilara <76774168+dilaracs@users.noreply.github.com>
2021-02-10 21:07:38 +0300

    Add files via upload

---
+++ Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -0,0 +1,14 @@
+﻿using Core.Entities;
+using DataAccess.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.DataAccess.EntityFramework
+{
+    public class EfEntityRepositoryBase<TEntity, TContext> : IEntityRepository<TEntity>
+        where TEntity : class, IEntity, new()
+        where TContext : DbContext, new()
+    {
+    }
+}
